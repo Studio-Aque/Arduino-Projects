6c5233b34d23cf15ce61cfb7fc95d6956fb6e65c
William Hayes <aque6130@gmail.com>
2024-05-27 19:21:01 -0600

    Update sketch_may23a.ino

--- Testing/sketch_may23a/sketch_may23a.ino
+++ Testing/sketch_may23a/sketch_may23a.ino
@@ -1,39 +1,40 @@
-int redLed = 11; //for detecting
-int yellowLed = 12; //for detecting
-int greenLed = 13; //for detecting
+int redLed = 11; //set pin for detecting
+int yellowLed = 12; //set pin for detecting
+int greenLed = 13; //set pin for detecting
 
 int crashSensor = 3// set pin for collision sensor
-;int crashVal;// set digital variable val
+;int crashVal;// variable for the crash one
 
 int interSensor = 2;
-;int interVal;
+;int interVal;// variable for the inter one 
 
-void setup()
+void setup() //setups
 { 
   pinMode(crashSensor, INPUT);// set collision sensor as input
-  pinMode(interSensor, INPUT);
+  pinMode(interSensor, INPUT);// the interupting sensor thinggy as an imput
 
-  pinMode(redLed, OUTPUT);
-  pinMode(yellowLed, OUTPUT);
-  pinMode(greenLed, OUTPUT);
+  pinMode(redLed, OUTPUT); //the red led
+  pinMode(yellowLed, OUTPUT); //the yellow led
+  pinMode(greenLed, OUTPUT); //infer what this led is 
 
 }
-void loop()
+
+void loop() //loops
 {
   crashVal = digitalRead(crashSensor);// read value on pin 3 and assign it to val
-  interVal = digitalRead(interSensor);
+  interVal = digitalRead(interSensor);// read above but change a couple things, not that hard
 
-  if (crashVal==HIGH) {
-    digitalWrite(redLed, LOW);
+  if (crashVal==HIGH) { //if the button isnt complete
+    digitalWrite(redLed, LOW); //turns off the led
+  }
+  else { //if it is detected
+    digitalWrite(redLed, HIGH); //try and guess
+  }
+
+  if (interVal == HIGH) { //really the one above but opposite, because math
+    digitalWrite(yellowLed, HIGH); //this does something
   }
   else {
-    digitalWrite(redLed, HIGH);
-  }
-
-  if (interVal == HIGH) {
-    digitalWrite(yellowLed, HIGH);
-  }
-  else {
-    digitalWrite(yellowLed, LOW);
+    digitalWrite(yellowLed, LOW); //so does this
   }
 }
